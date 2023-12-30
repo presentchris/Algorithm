edf0d5ab1740a88896d3c2b6eb08c6d8cb710527
누리 <140428477+presentchris@users.noreply.github.com>
2023-12-30 20:54:16 +0900

    [PCCE 기출문제] 2번 / 피타고라스의 정리
    <문제 설명>
    직각삼각형이 주어졌을 때 빗변의 제곱은 다른 두 변을 각각 제곱한 것의 합과 같습니다.
    
    직각삼각형의 한 변의 길이를 나타내는 정수 a와 빗변의 길이를 나타내는 정수 c가 주어질 때, 다른 한 변의 길이의 제곱, b_square 을 출력하도록 한 줄을 수정해 코드를 완성해 주세요.
    
    
    <제한사항>
    1 ≤ a < c ≤ 100
    
    
    <입출력 예>
    입력 #1
    3
    5
    출력 #1
    16
    
    
    <배운 점>
    제곱 연산자를 사용할 때 '**'을 사용하면 2제곱이 적용되는 줄 알았는데, '**2'처럼 지수를 지정해야 한다는 점을 배웠다.

---
+++ 제곱연산자_피타고라스의정리.py
@@ -0,0 +1,32 @@
+a = int(input())
+c = int(input())
+
+b_square = c**2 - a**2
+print(b_square)
+
+
+
+# 다른 사람의 풀이1
+# a = int(input())
+# c = int(input())
+
+# b_square = c * c - a * a
+# print(b_square)
+
+
+
+# 다른 사람의 풀이2
+# a = int(input())
+# c = int(input())
+
+# b_square =  abs(c**2 - a**2)
+# print(b_square)
+
+
+
+# 다른 사람의 풀이3
+# a = int(input())
+# c = int(input())
+
+# b_square = pow(c,2) - pow(a,2)
+# print(b_square)
