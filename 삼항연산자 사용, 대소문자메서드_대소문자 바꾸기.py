9e7a5ae74030d4b99c56e265ea9dede61afc7231
누리 <140428477+presentchris@users.noreply.github.com>
2024-01-05 13:52:55 +0900

    대소문자 바꿔서 출력하기_삼항연산자 사용, isupper(), islower(), upper(), lower() 등 사용
    <문제 설명>
    영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
    
    <제한사항>
    1 ≤ str의 길이 ≤ 20
    str은 알파벳으로 이루어진 문자열입니다.
    
    <입출력 예>
    입력 #1
    aBcDeFg
    출력 #1
    AbCdEfG
    
    <배운 점>
    isupper() 또는 islower()가 있는지 몰랐는데 설마 설마하면서 사용해봤지만 정말 있었다. 문자열과 리스트로 다양한 시도를 해보았고, 초기화 위치에 따라 결과가 달라진다는 점도 알게 되었다.
    그리고 스스로 삼항연산자를 사용해보며 간단한 코드 처리를 할 수 있다는 점을 배웠다. 삼항연산자를 통해 elif를 쓸 때에는 
    x = 10
    result = "Positive" if x > 0 else ("Zero" if x == 0 else "Negative")
    print(result)
    이런식으로 else와 괄호 처리를 해서 사용할 수 있다는 점도 배웠다.

---
+++ 삼항연산자 사용, 대소문자메서드_대소문자 바꾸기.py
@@ -0,0 +1,32 @@
+# 첫 번째로 시도해 본 코드
+# str = input()
+# for i in str:
+#     if i.isupper():
+#         print(i.lower(), end='')
+#     else:
+#         print(i.upper(), end='')
+
+# 두 번째로 시도해 본 코드. 리스트를 사용하여 append 해 봄. result_list를 사용하여 초기화할 때 for문에 들어가 있는지, 밖에 있는지의 위치에 따라 다른 결과가 출력됨.
+# str = input()
+# for i in str:
+#     result_list= []
+#     result_list.append(i.lower() if i.isupper() else i.upper())
+#     result = ''.join(result_list)
+#     print(result, end='')
+
+# 세 번째로 시도해 본 코드
+# str = input()
+# for i in str:
+#     result = ""
+#     result += i.lower() if i.isupper() else i.upper()
+#     print(result, end='')
+
+# 네 번째로 시도해 본 코드. 삼항 연산자를 사용함. 
+str = input()
+result = ''.join(i.lower() if i.isupper() else i.upper() for i in str)
+print(result)
+
+# 다섯 번째로 시도한 코드. 삼항 연산자를 사용하되, join을 빼면 출력은 되지만 리스트 형태로 출력됨. join을 넣어서 결과적으로 위와 같이 나옴.
+# str = input()
+# result = ''.join([i.lower() if i.isupper() else i.upper() for i in str])
+# print(result)
