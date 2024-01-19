49c9a61540a6a65c0cd033a17c8d46d0cf0039ab
박누리 <140428477+presentchris@users.noreply.github.com>
2024-01-19 13:00:21 +0900

    프로그래머스_n의 배수
    <문제 설명>
    정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return n의 배수가 아니라면 0을 return하도록 solution 함수를 완성해주세요.
    
    <제한사항>
    2 ≤ num ≤ 100
    2 ≤ n ≤ 9
    
    <입출력 예>
    #1
    98은 2의 배수이므로 1을 return합니다.
    #2
    32는 3의 배수가 아니므로 0을 return합니다.
    
    <배운 점>
    1초컷이라고 생각하며 풀었지만 더 다양한 생각을 하지 못하고 기계적으로 풀었던 것 같다. 쉽게 풀리는 문제든 어렵게 풀리는 문제든 다양한 풀이를 해보려고 시도해야겠다. 다른 사람의 풀이를 보며 not()을 쓰고 int()를 써서 bool 값을 활용하는 방법도 창의성이 돋보인다고 생각했고 뻔하지 않은 답이라 좋았다. 마찬가지로 int()만을 사용하는 답도 그랬다. not()은 조금 더 생각한 풀이이기 때문에 더 감명깊었다. 가능한 루트를 다양하게 생각하는 연습 또한 중요하다는 점을 배웠다.

---
+++ n의배수.py
@@ -0,0 +1,12 @@
+# 나의 풀이
+def solution(num, n):
+    return 1 if num%n==0 else 0
+
+# 다른 사람의 풀이1
+# def solution(num, n):
+#     return int(not(num % n))
+
+# 다른 사람의 풀이2
+# def solution(num, n):
+#     return int(num % n == 0)
+
