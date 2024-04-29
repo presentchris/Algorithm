24bf0bbdd0c46157726c77308ae396ef39344df5
박누리 <140428477+presentchris@users.noreply.github.com>
2024-04-29 13:22:09 +0900

    프로그래머스_글자 지우기
    <문제 설명>
    문자열 my_string과 정수 배열 indices가 주어질 때, my_string에서 indices의 원소에 해당하는 인덱스의 글자를 지우고 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.
    
    <제한사항>
    1 ≤ indices의 길이 < my_string의 길이 ≤ 100
    my_string은 영소문자로만 이루어져 있습니다
    0 ≤ indices의 원소 < my_string의 길이
    indices의 원소는 모두 서로 다릅니다.
    
    <입출력 예>
    my_string : "apporoograpemmemprs"
    indices : [1, 16, 6, 15, 0, 10, 11, 3]	result : "programmers"
    
    <배운 점>
    del이 문자열에서 쓸 수는 없으니까 my_string을 list로 바꾸어서 del을 사용한 풀이가 있었다. 그리고 strip뿐만 아니라  replace, re.sub 같은 함수도 해당되는 문자를 모두 지운다는 것을 알았다. 이런 경우에는 그냥 간단하게 슬라이싱하여 문자열을 이어붙이면 된다. not in 을 사용하지 않고 이어붙이기만 하면 에러가 났다. 해당 인덱스의 문자를 삭제하고 붙이는 방법보다는 해당 인덱스의 문자를 뛰어넘는 것이 훨씬 효율적이다. 다른 사람들의 풀이를 보며 다시 한 번 감을 잡아야겠다.

--- 글자 지우기.py
+++ 글자 지우기.py
@@ -1,8 +1,9 @@
 '''
-사용 가능한 함수 및 로직
+글자를 지울 때 사용 가능한 함수 및 로직
 1. replace
 2. re.sub
-*strip, rstrip, lstrip 등은 해당하는 문자를 모두 지우기 때문에 이 경우에 사용할 수는 없을 것
+3. strip(양끝단 / 위치에 따라 rstrip, lstrip)
+*strip, rstrip, lstrip 등과 위 함수들은 해당하는 문자를 모두 지우기 때문에 이 경우에 사용할 수는 없을 것
 
 # 예1
 def solution(my_string, indices):    
@@ -16,3 +17,28 @@ def solution(my_string, indices):
 '''
 
 # 내 풀이1
+def solution(my_string, indices):
+    ans = ''
+    for i in range(len(my_string)):
+        if i not in indices:
+            ans += my_string[i]
+    return ans
+
+# 다른 사람의 풀이1
+def solution(my_string, indices):
+    my_string = list(my_string)
+    for i in sorted(indices, reverse=True):
+        del my_string[i]
+    return ''.join(my_string)
+
+
+# 다른 사람의 풀이2
+def solution(my_string, indices):
+    return "".join([v for i,v in enumerate(my_string) if i not in indices])
+
+
+# 다른 사람의 풀이3_replace 안 쓰고 X를 ''로 바로 바꿀 수 있음
+def solution(my_string, indices):
+    for idx in indices:
+        my_string = my_string[:idx] + 'X' + my_string[idx+1:]
+    return my_string.replace('X', '')
